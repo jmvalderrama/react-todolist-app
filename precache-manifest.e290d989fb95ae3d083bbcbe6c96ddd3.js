3b84eeab4d8464c5dd899bd74fa10863814638a6
Juan Mario Valderrama Gonzalez <juancho9402@hotmail.com>
2019-01-12 17:08:00 -0500

    Updates

---
+++ precache-manifest.e290d989fb95ae3d083bbcbe6c96ddd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6925251f480db203a770",
+    "url": "/react-todolist-app/static/js/runtime~main.6925251f.js"
+  },
+  {
+    "revision": "5615c86edd61dc4e2013",
+    "url": "/react-todolist-app/static/js/main.5615c86e.chunk.js"
+  },
+  {
+    "revision": "6f96e78ba8e11d79daff",
+    "url": "/react-todolist-app/static/js/1.6f96e78b.chunk.js"
+  },
+  {
+    "revision": "5615c86edd61dc4e2013",
+    "url": "/react-todolist-app/static/css/main.938829a8.chunk.css"
+  },
+  {
+    "revision": "021adc96082ff436933ce4d7984e65f5",
+    "url": "/react-todolist-app/index.html"
+  }
+];
